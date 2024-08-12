7cfa395c141a96bef88009bc51826a9e81247bb2
Elvis Cheng (ESD - Software Trainee, Digital Solutions) <elvistfcheng@esdlife.com>
2024-08-12 17:23:54 +0800

    Initial Commit

---
+++ BeeNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeeNote
+//
+//  Created by Elvis Cheng (ESD - Software Trainee, Digital Solutions) on 12/8/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
